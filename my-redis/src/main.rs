d64b2bf04c9faadbec02ae08a51058bfcb0a9250
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-02-01 09:16:41 +0000

    using std mutex

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -1,25 +1,33 @@
+use std::{collections::HashMap, sync::{Arc, Mutex}};
+use bytes::Bytes;
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
 
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main(){
     // bind the listener to the address
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    println!("Listening");
+    let db = Arc::new(Mutex::new(HashMap::new()));
     loop{
         // the second items contains the IP and port of the new connection.
         let (socket, _)= listener.accept().await.unwrap();
+        // clone just the handle
+        let db = db.clone();
+        println!("Accepted");
+
         tokio::spawn(async move{
             // green thread https://en.wikipedia.org/wiki/Green_thread
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket:TcpStream){
+async fn process(socket:TcpStream, db:Db){
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-    let mut db =HashMap::new();
+    
 
     // the 'connection' lets us read/write redis **frames** instead of byte streams. The 'Connection' type is defined by mini-redis.
     let mut connection = Connection::new(socket);
@@ -28,15 +36,17 @@ async fn process(socket:TcpStream){
 
         let response = match Command::from_frame(frame).unwrap(){
             Set(cmd)=>{
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd)=>{
+                let db = db.lock().unwrap();
                 if let Some(value) =db.get(cmd.key()){
                     // 'Frame::Bulk' expects data to be of type 'Bytes'. 
                     // will be covered later in the tutorial. 
                     // for now '&Vec<u8>' is converted to 'Bytes' using 'into()'
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 }else{
                     Frame::Null
                 }
